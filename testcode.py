b0cd07c57225a3927f8a05d93022d5c3e5e0b075
Null_MainPC <jp128hw@gmail.com>
2024-05-12 16:42:29 -0600

    hi

---
+++ testcode.py
@@ -0,0 +1,13 @@
+import pygame
+from pygame.locals import *
+import random
+import math
+import sys
+
+'''Test maincode'''
+
+'''Window propierties'''
+BACKGROUND=(255,255,255)
+window_x=720
+windoy_y=720
+win
