6840b87050856cbed2d3e469d668790e96c5c3ce
Null_MainPC <jp128hw@gmail.com>
2024-05-13 14:56:40 -0600

    Test 1

--- testcode.py
+++ testcode.py
@@ -6,8 +6,27 @@ import sys
 
 '''Test maincode'''
 
+'''pygame init'''
+pygame.init()
+
 '''Window propierties'''
 BACKGROUND=(255,255,255)
-window_x=720
-windoy_y=720
-win
+window_x=1020
+window_y=640
+FPS=60
+window=pygame.display.set_mode((window_x,window_y))
+clock = pygame.time.Clock()
+
+'''Pre-defined colors'''
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+
+while True:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    window.fill(BACKGROUND)
+    pygame.display.update()
+    clock.tick(FPS)
