95d44799e34ff05f293d1b2f6032db5e4f9ac4d1
Null_MainPC <jp128hw@gmail.com>
2024-05-13 17:22:18 -0600

    New by-default ground and a box addedf

--- testcode.py
+++ testcode.py
@@ -4,8 +4,6 @@ import random
 import math
 import sys
 
-'''Test maincode'''
-
 '''pygame init'''
 pygame.init()
 
@@ -16,6 +14,18 @@ window_y=640
 FPS=60
 window=pygame.display.set_mode((window_x,window_y))
 clock = pygame.time.Clock()
+
+'''By-default ground'''
+ground_height=120
+startground_x=0
+startground_y=window_y-ground_height
+endground_x=window_x
+endground_y=window_y-ground_height
+
+'''Testing'''
+box_shape_xy=200
+absolute_starting_box_xpos=300
+absolute_starting_box_ypos=window_y-ground_height-box_shape_xy
 
 '''Pre-defined colors'''
 BLACK = (0,0,0)
@@ -28,5 +38,7 @@ while True:
             sys.exit()
 
     window.fill(BACKGROUND)
+    pygame.draw.line(window,BLACK,(startground_x,startground_y),(endground_x,endground_y),3)
+    pygame.draw.rect(window,BLACK,((absolute_starting_box_xpos,absolute_starting_box_ypos),(box_shape_xy,box_shape_xy)),3)
     pygame.display.update()
     clock.tick(FPS)
